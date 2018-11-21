d21a75aca4ba2eb9fe2929d24bcf42b313445379
Marie Burns <mcburns2222@gmail.com>
2018-11-21 21:59:50 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -81,9 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] = "asf"
-
-puts programmer_hash
+programmer_hash[:yukihiro_matsumoto]
     
 end
 
